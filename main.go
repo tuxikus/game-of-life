aeeb80bf9b2d3dbc296f6cf04d0c00ff84725862
tuxikus <contact@tuxikus.de>
2025-08-09 13:08:09 +0200

    added better ui with buttons

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 )
 
 const (
-	WIN_X                = 1000
-	WIN_Y                = 600
+	WIN_X                = 1000 // game area
+	WIN_Y                = 600  // game area
+	UI_Y                 = 200
 	GRID_TILE_SIZE       = 25
 	GRID_TILE_LINE_WIDTH = 1
 
@@ -34,9 +35,22 @@ type Cell struct {
 
 type Game struct {
 	zoom       float32
+	font       rl.Font
+	speed      int
 	cells      [ROWS][COLS]Cell
 	generation int
 	state      int
+	buttons    []Button
+}
+
+type Button struct {
+	pos        rl.Vector2
+	size       rl.Vector2
+	color      rl.Color
+	hoverColor rl.Color
+	fontColor  rl.Color
+	text       string
+	action     func()
 }
 
 var game = Game{}
@@ -44,12 +58,73 @@ var game = Game{}
 func initalize() {
 	game.zoom = 1.0
 	game.state = STOPPED
+	game.font = rl.LoadFont("./font/static/OpenSans-Bold.ttf")
+	game.speed = 60
 
 	for y := 0; y < ROWS; y++ {
 		for x := 0; x < COLS; x++ {
 			game.cells[y][x] = Cell{state: DEAD}
 		}
 	}
+
+	game.buttons = []Button{
+		{
+			pos:        rl.Vector2{X: 5, Y: WIN_Y + 10},
+			size:       rl.Vector2{X: 200, Y: 50},
+			color:      rl.Gray,
+			hoverColor: rl.LightGray,
+			fontColor:  rl.Black,
+			text:       "Run/Stop",
+			action:     changeGameState,
+		},
+		{
+			pos:        rl.Vector2{X: 5, Y: WIN_Y + 10 + 60},
+			size:       rl.Vector2{X: 90, Y: 50},
+			color:      rl.Gray,
+			hoverColor: rl.LightGray,
+			fontColor:  rl.Black,
+			text:       "+",
+			action:     increaseSpeed,
+		},
+		{
+			pos:        rl.Vector2{X: 115, Y: WIN_Y + 10 + 60},
+			size:       rl.Vector2{X: 90, Y: 50},
+			color:      rl.Gray,
+			hoverColor: rl.LightGray,
+			fontColor:  rl.Black,
+			text:       "-",
+			action:     decreaseSpeed,
+		},
+		{
+			pos:        rl.Vector2{X: 5, Y: WIN_Y + 10 + 60*2},
+			size:       rl.Vector2{X: 200, Y: 50},
+			color:      rl.Gray,
+			hoverColor: rl.LightGray,
+			fontColor:  rl.Black,
+			text:       "Next Gen.",
+			action:     nextGeneration,
+		},
+	}
+}
+
+func increaseSpeed() {
+	new := game.speed + 10
+
+	if new > 120 {
+		return
+	}
+
+	game.speed = new
+}
+
+func decreaseSpeed() {
+	new := game.speed - 10
+
+	if new < 1 {
+		return
+	}
+
+	game.speed = new
 }
 
 func spawn() {
@@ -60,6 +135,10 @@ func spawn() {
 
 		x := int(mousePosX / GRID_TILE_SIZE)
 		y := int(mousePosY / GRID_TILE_SIZE)
+
+		if x >= COLS || y >= ROWS {
+			return
+		}
 
 		game.cells[y][x].state = ALIVE
 	}
@@ -152,6 +231,39 @@ func drawCells() {
 	}
 }
 
+func drawButtons() {
+	mousePos := rl.GetMousePosition()
+	mouseX := mousePos.X
+	mouseY := mousePos.Y
+	var fontSize float32 = 32.0
+	var spacing float32 = 2.0
+
+	for _, button := range game.buttons {
+		if mouseX > button.pos.X && mouseX < button.pos.X+button.size.X &&
+			mouseY > button.pos.Y && mouseY < button.pos.Y+button.size.Y {
+			rl.DrawRectangleV(button.pos, button.size, button.hoverColor)
+
+			if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+				button.action()
+			}
+		} else {
+			rl.DrawRectangleV(button.pos, button.size, button.color)
+		}
+
+		rl.DrawTextEx(
+			game.font,
+			button.text,
+			rl.Vector2{
+				X: button.pos.X + button.size.X/2 - rl.MeasureTextEx(game.font, button.text, fontSize, spacing).X/2,
+				Y: button.pos.Y + button.size.Y/2 - rl.MeasureTextEx(game.font, button.text, fontSize, spacing).Y/2,
+			},
+			fontSize,
+			spacing,
+			button.fontColor,
+		)
+	}
+}
+
 func drawUI() {
 	state := ""
 	switch game.state {
@@ -163,26 +275,68 @@ func drawUI() {
 
 	genStr := fmt.Sprintf("Generation: %d", game.generation)
 	stateStr := fmt.Sprintf("State: %s", state)
-	fontSize := 24
+	speedStr := fmt.Sprintf("Speed: %d", game.speed)
+	var fontSize float32 = 32.0
+	var spacing float32 = 2.0
 
-	rl.DrawText(stateStr, WIN_X-rl.MeasureText(stateStr, int32(fontSize))-5, 5, int32(fontSize), rl.Black)
-	rl.DrawText(genStr, WIN_X-rl.MeasureText(genStr, int32(fontSize))-5, 30, int32(fontSize), rl.Black)
+	// top line in ui area
+	rl.DrawLineEx(rl.Vector2{X: 0, Y: WIN_Y}, rl.Vector2{X: WIN_X, Y: WIN_Y}, 3.0, rl.Black)
+
+	rl.DrawTextEx(
+		game.font,
+		stateStr,
+		rl.Vector2{
+			X: 215,
+			Y: WIN_Y + 5,
+		},
+		fontSize,
+		spacing,
+		rl.Black)
+
+	rl.DrawTextEx(
+		game.font,
+		speedStr,
+		rl.Vector2{
+			X: 215,
+			Y: WIN_Y + 5 + 60,
+		},
+		fontSize,
+		spacing,
+		rl.Black)
+
+	rl.DrawTextEx(
+		game.font,
+		genStr,
+		rl.Vector2{
+			X: 215,
+			Y: WIN_Y + 5 + 60*2,
+		},
+		fontSize,
+		spacing,
+		rl.Black)
+
+	drawButtons()
+}
+
+func changeGameState() {
+	switch game.state {
+	case STOPPED:
+		game.state = RUNNING
+	case RUNNING:
+		game.state = STOPPED
+	}
 }
 
 func main() {
-	initalize()
-	rl.InitWindow(WIN_X, WIN_Y, "game of life")
+	rl.InitWindow(WIN_X, WIN_Y+UI_Y, "game of life")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(60)
+	initalize()
+
+	rl.SetTargetFPS(120)
 	fc := 1
 
 	for !rl.WindowShouldClose() {
-		if rl.IsKeyPressed(rl.KeyF1) && game.state == STOPPED {
-			game.state = RUNNING
-		} else if rl.IsKeyPressed(rl.KeyF1) && game.state == RUNNING {
-			game.state = STOPPED
-		}
 
 		if game.state == RUNNING {
 			fc++
@@ -191,7 +345,7 @@ func main() {
 		// processMousewheelInput()
 		spawn()
 
-		if fc%10 == 0 {
+		if fc%game.speed == 0 {
 			processGeneration()
 		}
 
@@ -200,7 +354,7 @@ func main() {
 		rl.ClearBackground(rl.RayWhite)
 
 		drawCells()
-		// drawGrid()
+		drawGrid()
 		drawUI()
 
 		rl.EndDrawing()
